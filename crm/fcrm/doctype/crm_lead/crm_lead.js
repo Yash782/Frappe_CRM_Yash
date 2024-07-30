83a5c5d2c5a7be2b7d9c9f25f9fc2e27b03ac9e3
Yash Wadgaonkar <wadgaonkaryash782@gmail.com>
2024-07-30 18:06:40 +0530

    minor changes

--- crm/fcrm/doctype/crm_lead/crm_lead.js
+++ crm/fcrm/doctype/crm_lead/crm_lead.js
@@ -2,7 +2,37 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("CRM Lead", {
-	refresh(frm) {
-		frm.add_web_link(`/crm/leads/${frm.doc.name}`, __("Open in Portal"));
-	},
+    refresh(frm) {
+        frm.add_web_link(`/crm/leads/${frm.doc.name}`, __("Open in Portal"));
+        frm.add_custom_button(__('Call Lead'), function() {
+            let phone_number = frm.doc.mobile_no;
+ 
+            if (phone_number) {
+                frappe.call({
+                    method: 'smart_flo.api.fetch_agent.get_agent_info',
+                    args: {
+                        phone_number: phone_number
+                    },
+                    callback: function(response) {
+                        const result = response.message;
+                        if (result.message) {
+                            frappe.msgprint({
+                                title: __('Error'),
+                                message: result.message,
+                                indicator: 'red'
+                            });
+                        } else {
+                            frappe.msgprint({
+                                title: __('Success'),
+                                message: __('Call initiated successfully: ') + JSON.stringify(result),
+                                indicator: 'green'
+                            });
+                        }
+                    }
+                });
+            } else {
+                frappe.msgprint(__('Phone number is missing.'));
+            }
+        });
+    }
 });
